a93bcfdef257fe77f15cd067895eedee4ad6f84a
Mevan Samaratunga <mevansam@gmail.com>
2023-03-17 11:40:43 -0400

    ensures resource lists are parsed by cb cli

--- cloud/recipes/server/docker/docker-input.tf
+++ cloud/recipes/server/docker/docker-input.tf
@@ -1,6 +1,10 @@
 #
-# AWS specific resource names
+# Docker specific resource names
 #
 # @resource_instance_list: docker_container.minecraft,docker_image.minecraft
 # @resource_instance_data_list: 
 #
+
+variable "__place_holder__" {
+  default = ""
+}
